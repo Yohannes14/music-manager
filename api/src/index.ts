b7c1630bcefe9b643484bc454409ac2f82c46f37
Yohannes14 <yohansdegu.2010@gmail.com>
2024-02-07 17:33:49 +0300

    docker

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import router from './routes/routes';
 import { config } from './config/config';
+import { SongModal } from './models/song';
 
 const app = express();
 app.use(express.json());
@@ -26,6 +27,83 @@ async function connectToDatabase() {
 }
 // Call the connectToDatabase function
 connectToDatabase();
+
+
+const ethiopianMusicList = [
+    {
+        title: 'Ene Negn Yeshi',
+        artist: 'Gossaye Tesfaye',
+        album: 'Ene Negn Yeshi',
+        genre: 'Ethiopian',
+    },
+    {
+        title: 'Tew Man Necheh',
+        artist: 'Mahmoud Ahmed',
+        album: 'Tew Man Necheh',
+        genre: 'Ethio-Jazz',
+    },
+    {
+        title: 'Belomi Benna',
+        artist: 'Tilahun Gessesse',
+        album: 'Belomi Benna',
+        genre: 'Traditional',
+    },
+    {
+        title: 'Yene Felagote',
+        artist: 'Aster Aweke',
+        album: 'Kabu',
+        genre: 'Pop',
+    },
+    {
+        title: 'Yeberhaw Guzo',
+        artist: 'Mulatu Astatke',
+        album: 'Yekatit Ethio Jazz',
+        genre: 'Ethio-Jazz',
+    },
+    {
+        title: 'Sost Tsion',
+        artist: 'Gigi Shibabaw',
+        album: 'Gigi',
+        genre: 'World',
+    },
+    {
+        title: 'Yene Mar',
+        artist: 'Teddy Afro',
+        album: 'Yasteseryal',
+        genre: 'Reggae',
+    },
+    {
+        title: 'Yi Godellel',
+        artist: 'Mohammed Ahmed',
+        album: 'Ere Mela Mela',
+        genre: 'Ethio-Jazz',
+    },
+    {
+        title: 'Hode Baba',
+        artist: 'Ezra Kibret',
+        album: 'Ezra Kibret',
+        genre: 'Folk',
+    },
+    {
+        title: 'Hageren',
+        artist: 'Girma Beyene',
+        album: 'Ethiopian Urban Modern Music Vol. 5',
+        genre: 'Ethio-Jazz',
+    },
+];
+const insertEthiopianMusicData = async () => {
+    try {
+        // Insert the Ethiopian music list data to MongoDB
+        const result = await SongModal.insertMany(ethiopianMusicList);
+        console.log(`${result.length} documents inserted successfully into MongoDB.`);
+    } catch (error) {
+        console.error('Error inserting Ethiopian music data to MongoDB:', error);
+    }
+};
+
+// Call the function to insert data
+insertEthiopianMusicData();
+
 // Define the port from the configuration
 const port: number = config.port;
 
